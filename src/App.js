6f90bd324523d1d6d52349363c6d5a18f98deea3
themikail <hello@mikailgokce.com>
2023-07-18 03:45:18 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "./App.css";
-import CalendarComponent from "./components/calendarComp";
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import CalendarComp from "./components/calendarComp";
+import Login from "./components/auth/login";
+import SignUp from "./components/auth/signup";
+import { AuthProvider } from "./components/auth/authContext";
+import PrivateRoute from "./components/auth/privateRoute";
+
+const App = () => {
   return (
-    <div className="App">
-      <CalendarComponent />
-    </div>
+    <AuthProvider>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Register</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* PrivateRoute innerhalb von Routes */}
+            <PrivateRoute path="/" element={<CalendarComp />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
